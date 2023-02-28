c6e592e21230ddd96a9910993944b93124bf1aea
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-03-01 00:30:38 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/GreyMatter_Bots')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/ary_botz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
